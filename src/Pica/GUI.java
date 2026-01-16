aae74f9b1315c576d3a08a3f8f216573dd799e29
Kolab0k <bogdan1ptlvt@gmail.com>
2026-01-16 16:05:13 +0300

    Uzlaboju kļudu

--- src/Pica/GUI.java
+++ src/Pica/GUI.java
@@ -1,33 +1,40 @@
 package Pica;
 
 import java.awt.BorderLayout;
-
+import java.awt.Color;
+import java.awt.Font;
+import java.net.URL;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 public class GUI {
 
-	public static void main(String[] args) {
-		
-		JFrame frame = new JFrame("Picerija");
+    public static void main(String[] args) {
+        
+        JFrame frame = new JFrame("Picerija");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 600);
         frame.setLocationRelativeTo(null); 
         
-        ImageIcon backgroundIcon = new ImageIcon("GIF/background.gif");
-        JLabel background = new JLabel(backgroundIcon);
-        background.setLayout(new BorderLayout());
-        frame.setContentPane(background);
+        URL imgUrl = GUI.class.getResource("/GIF/background.gif");
+        
+        if (imgUrl != null) {
+            ImageIcon backgroundIcon = new ImageIcon(imgUrl);
+            JLabel background = new JLabel(backgroundIcon);
+            background.setLayout(new BorderLayout());
+            frame.setContentPane(background);
+        } else {
+            System.err.println("Kļuda: Fails nav atrsts : src/GIF/background.gif");
+            frame.getContentPane().setBackground(Color.DARK_GRAY);
+        }
         
         JLabel label = new JLabel("Welcome!", JLabel.CENTER);
-        label.setFont(new java.awt.Font("Arial", java.awt.Font.BOLD, 30));
-        label.setForeground(java.awt.Color.WHITE);
+        label.setFont(new Font("Arial", Font.BOLD, 40));
+        label.setForeground(Color.WHITE);
         
-        frame.getContentPane().add(label);
+        frame.add(label);
 
         frame.setVisible(true);
-
-	}
-
+    }
 }
