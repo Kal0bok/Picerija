d5b02b6dafde4564399880466feb278d3e6595a6
Kolab0k <bogdan1ptlvt@gmail.com>
2026-01-16 16:23:15 +0300

    Astartpe pogam

--- src/Pica/GUI.java
+++ src/Pica/GUI.java
@@ -1,13 +1,11 @@
 package Pica;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.net.URL;
-
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -24,37 +22,42 @@ public class GUI {
         
         URL imgUrl = GUI.class.getResource("/GIF/background.gif");
         
-        if (imgUrl != null) {
-            ImageIcon backgroundIcon = new ImageIcon(imgUrl);
-            JLabel background = new JLabel(backgroundIcon);
-            background.setLayout(new BorderLayout());
-            frame.setContentPane(background);
-        } else {
-            System.err.println("Kļuda: Fails nav atrsts : src/GIF/background.gif");
-            frame.getContentPane().setBackground(Color.DARK_GRAY);
-        }
+        JLabel background = new JLabel();
         
+        if (imgUrl != null) {
+            background.setIcon(new ImageIcon(imgUrl));
+        } else {
+            System.err.println("Kļūda: Fails nav atrasts : /GIF/background.gif");
+            background.setOpaque(true);
+            background.setBackground(Color.DARK_GRAY);
+        }
+
         background.setLayout(new GridBagLayout());
         frame.setContentPane(background);
 
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.gridx = 0; 
-        gbc.insets = new Insets(10, 10, 10, 10); 
+        gbc.insets = new Insets(15, 10, 15, 10); 
         gbc.anchor = GridBagConstraints.CENTER; 
+        gbc.fill = GridBagConstraints.HORIZONTAL; 
 
-        JLabel label = new JLabel("Welcome!");
+        JLabel label = new JLabel("Welcome!", JLabel.CENTER);
         label.setFont(new Font("Arial", Font.BOLD, 40));
         label.setForeground(Color.WHITE);
         gbc.gridy = 0; 
+        gbc.weighty = 5; 
         background.add(label, gbc);
 
+        gbc.weighty = 0;
         JButton startButton = new JButton("Sākt darba maiņu");
         startButton.setFont(new Font("Arial", Font.PLAIN, 20));
-        gbc.gridy = 1; 
+        startButton.setFocusable(false); 
+        gbc.gridy = 0; 
         background.add(startButton, gbc);
 
         JButton exitButton = new JButton("Iziet");
         exitButton.setFont(new Font("Arial", Font.PLAIN, 20));
+        exitButton.setFocusable(false);
         gbc.gridy = 2; 
         background.add(exitButton, gbc);
         
