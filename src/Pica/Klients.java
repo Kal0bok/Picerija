abba028ea2cbfa4bc857c21b96d5e9aef952b1ea
Kolab0k <bogdan1ptlvt@gmail.com>
2026-01-17 01:46:48 +0300

    Pārnesu chat metodi uz darbnieka klasi

--- src/Pica/Klients.java
+++ src/Pica/Klients.java
@@ -1,41 +1,9 @@
 package Pica;
 
-import java.util.Timer;
-import java.util.TimerTask;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
 
 public class Klients {
 
     public static void main(String[] args) {
-        sakumsSveiciens("Labdien! Laipni lūdzam picērijā!");
-    }
-
-    public static void sakumsSveiciens(String teksts) {
-        JFrame frame = new JFrame("Picerija");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 300);
-        frame.setLocationRelativeTo(null);
-
-        JLabel label = new JLabel("", JLabel.CENTER);
-        frame.getContentPane().add(label);
-        frame.setVisible(true);
-
-        char[] chat = teksts.toCharArray();
-        final int[] index = {0}; 
-        Timer timer = new Timer();
-        TimerTask task = new TimerTask() {
-            @Override
-            public void run() {
-                if (index[0] < chat.length) {
-                    label.setText(label.getText() + chat[index[0]]);
-                    index[0]++;
-                } else {
-                    timer.cancel();
-                }
-            }
-        };
-        
-        timer.scheduleAtFixedRate(task, 500, 200);
+    
     }
 }
