4edccc1ac9f704e645a6cc5019c178390d178363
Kolab0k <bogdan1ptlvt@gmail.com>
2026-01-17 01:52:08 +0300

    Mazliet parveidoju kodu, lai tas būtu ka metode

--- src/Pica/Darbnieks.java
+++ src/Pica/Darbnieks.java
@@ -4,38 +4,32 @@ import java.util.Timer;
 import java.util.TimerTask;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
 
 public class Darbnieks {
 
     public static void main(String[] args) {
-        sakumsSveiciens("Labdien! Laipni lūdzam picērijā!");
+        
     }
 
-    public static void sakumsSveiciens(String teksts) {
-        JFrame frame = new JFrame("Picerija");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 300);
-        frame.setLocationRelativeTo(null);
-
-        JLabel label = new JLabel("", JLabel.CENTER);
-        frame.getContentPane().add(label);
-        frame.setVisible(true);
-
-        char[] chat = teksts.toCharArray();
-        final int[] index = {0}; 
+    public static void animateText(JLabel label, String teksts) {
+        char[] characters = teksts.toCharArray();
+        final int[] index = {0};
+        
         Timer timer = new Timer();
         TimerTask task = new TimerTask() {
             @Override
             public void run() {
-                if (index[0] < chat.length) {
-                    label.setText(label.getText() + chat[index[0]]);
-                    index[0]++;
+                if (index[0] < characters.length) {
+                    SwingUtilities.invokeLater(() -> {
+                        label.setText(label.getText() + characters[index[0]]);
+                        index[0]++;
+                    });
                 } else {
                     timer.cancel();
                 }
             }
         };
-        
-        timer.scheduleAtFixedRate(task, 500, 200);
+        timer.scheduleAtFixedRate(task, 3000, 100);
     }
 }
