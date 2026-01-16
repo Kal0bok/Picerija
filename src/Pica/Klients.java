eb188039f42d2457fb2a89d677267c242d7ecf53
Kolab0k <bogdan1ptlvt@gmail.com>
2026-01-17 01:46:04 +0300

    Parveidoju par klasi chatu

--- src/Pica/Klients.java
+++ src/Pica/Klients.java
@@ -2,42 +2,40 @@ package Pica;
 
 import java.util.Timer;
 import java.util.TimerTask;
-
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 public class Klients {
-		
-		public static String vards = "Ladbien!";
-	    public static char[] chat = vards.toCharArray();
-	    private static int index = 0;
 
-	    public static void main(String[] args) {
-	        JFrame frame = new JFrame("Picerija");
-	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	        frame.setSize(400, 300);
-	        frame.setLocationRelativeTo(null);
-	        
-	        JLabel label = new JLabel("", JLabel.CENTER);
-	        frame.getContentPane().add(label);
-	        frame.setVisible(true);
+    public static void main(String[] args) {
+        sakumsSveiciens("Labdien! Laipni lūdzam picērijā!");
+    }
 
-	        Timer timer = new Timer();
+    public static void sakumsSveiciens(String teksts) {
+        JFrame frame = new JFrame("Picerija");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 300);
+        frame.setLocationRelativeTo(null);
 
-	        TimerTask task = new TimerTask() {
-	            @Override
-	            public void run() {
-	                if (index < chat.length) {
-	                	label.setText(label.getText() + chat[index]);
-	                    index++;
-	                } else {
-	                    timer.cancel();
-	                }
-	            }
-	        };
-	        timer.scheduleAtFixedRate(task, 500, 200);
-	    
+        JLabel label = new JLabel("", JLabel.CENTER);
+        frame.getContentPane().add(label);
+        frame.setVisible(true);
 
-	}
-
+        char[] chat = teksts.toCharArray();
+        final int[] index = {0}; 
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                if (index[0] < chat.length) {
+                    label.setText(label.getText() + chat[index[0]]);
+                    index[0]++;
+                } else {
+                    timer.cancel();
+                }
+            }
+        };
+        
+        timer.scheduleAtFixedRate(task, 500, 200);
+    }
 }
