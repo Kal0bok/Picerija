5656c3d333f4ca26902fd4e32060dc637ce2bb9e
Kolab0k <bogdan1ptlvt@gmail.com>
2026-01-16 15:20:40 +0300

    Pilnīgi pabeidzu taimeru un parvietoju uz laiku

--- src/Pica/GalvenaKlase.java
+++ src/Pica/GalvenaKlase.java
@@ -1,37 +1,12 @@
 package Pica;
 
-import java.util.Timer;
-import java.util.TimerTask;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
 
 public class GalvenaKlase {
-    public static String[] vards = {"L", "a", "b", "d", "i", "e", "n", "!"};
-    private static int index = 0;
+	
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Picerija");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 300);
-        frame.setLocationRelativeTo(null);
-        
-        JLabel label = new JLabel("", JLabel.CENTER);
-        frame.getContentPane().add(label);
-        frame.setVisible(true);
-
-        Timer timer = new Timer();
-
-        TimerTask task = new TimerTask() {
-            @Override
-            public void run() {
-                if (index < vards.length) {
-                	label.setText(label.getText() + vards[index]);
-                    index++;
-                } else {
-                    timer.cancel();
-                }
-            }
-        };
-        timer.scheduleAtFixedRate(task, 500, 200);
+    	
+    	
+    	
     }
 }
--- src/Pica/Klients.java
+++ src/Pica/Klients.java
@@ -1,9 +1,42 @@
 package Pica;
 
-public class Klients {
+import java.util.Timer;
+import java.util.TimerTask;
 
-	public static void main(String[] args) {
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Klients {
 		
+		public static String vards = "Ladbien!";
+	    public static char[] chat = vards.toCharArray();
+	    private static int index = 0;
+
+	    public static void main(String[] args) {
+	        JFrame frame = new JFrame("Picerija");
+	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	        frame.setSize(400, 300);
+	        frame.setLocationRelativeTo(null);
+	        
+	        JLabel label = new JLabel("", JLabel.CENTER);
+	        frame.getContentPane().add(label);
+	        frame.setVisible(true);
+
+	        Timer timer = new Timer();
+
+	        TimerTask task = new TimerTask() {
+	            @Override
+	            public void run() {
+	                if (index < chat.length) {
+	                	label.setText(label.getText() + chat[index]);
+	                    index++;
+	                } else {
+	                    timer.cancel();
+	                }
+	            }
+	        };
+	        timer.scheduleAtFixedRate(task, 500, 200);
+	    
 
 	}
 
