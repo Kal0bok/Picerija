d9336e156cf860c3882385418f9bed9057a0b2d5
Kolab0k <bogdan1ptlvt@gmail.com>
2026-01-16 15:09:06 +0300

    Mazliet uzlaboju lai strada korekti taimers

--- src/Pica/GalvenaKlase.java
+++ src/Pica/GalvenaKlase.java
@@ -2,40 +2,37 @@ package Pica;
 
 import java.util.Timer;
 import java.util.TimerTask;
-
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 public class GalvenaKlase {
-	public String[] vards = {"Labdien"};
-	
-	public String chat() {
-	for(int i=0; i<vards.length;i++) {
-		timer.scheduleAtFixedRate(vards, 0, 0.5);
-	}
-	return null;
-	}
-	
-	public static void main(String[] args) {
-		
-		Timer timer = new Timer(); 
+    public static String[] vards = {"L", "a", "b", "d", "i", "e", "n", "!"};
+    private static int index = 0;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Picerija");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 300);
+        frame.setLocationRelativeTo(null);
+        
+        JLabel label = new JLabel("Welcome!", JLabel.CENTER);
+        frame.getContentPane().add(label);
+        frame.setVisible(true);
+
+        Timer timer = new Timer();
 
         TimerTask task = new TimerTask() {
             @Override
             public void run() {
-                System.out.println("Izpildits"); 
+                if (index < vards.length) {
+                    label.setText(vards[index]);
+                    System.out.println("Izpildits: " + vards[index]);
+                    index++;
+                } else {
+                    index = 0; 
+                }
             }
         };
-		
-		JFrame frame = new JFrame("Picerija");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 300);
-        frame.setLocationRelativeTo(null);
-        JLabel label = new JLabel("Welcome!", JLabel.CENTER);
-        frame.getContentPane().add(label);
-
-        frame.setVisible(true);
-
-	}
-
+        timer.scheduleAtFixedRate(task, 1000, 2000);
+    }
 }
