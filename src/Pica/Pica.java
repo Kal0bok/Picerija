c5bb92bb806bd4a1180c5a005c6fb1d862e1a4a5
Kolab0k <bogdan1ptlvt@gmail.com>
2026-01-17 20:06:01 +0300

    Izveidoju klasi pica

---
+++ src/Pica/Pica.java
@@ -0,0 +1,12 @@
+package Pica;
+
+public class Pica {
+
+	String nosaukums;
+    double cena;
+
+    public Pica(String nosaukums, double cena) {
+        this.nosaukums = nosaukums;
+        this.cena = cena;
+    }
+}
