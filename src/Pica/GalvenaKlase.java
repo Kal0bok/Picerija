9fce0b2d35dfe0e82296c5f98d16229bbb162838
Kolab0k <bogdan1ptlvt@gmail.com>
2026-01-18 02:20:47 +0300

    Masīvi ar datiem

--- src/Pica/GalvenaKlase.java
+++ src/Pica/GalvenaKlase.java
@@ -1,10 +1,17 @@
 package Pica;
 
+import java.util.Random;
+
 public class GalvenaKlase {
 	
-    public static void main(String[] args) {
-    	
-    	
-    	
-    }
+	private static Random rand = new Random();
+
+    private static final String[] VĀRDI = {"Nikita", "Artjoms", "Jānis", "Maksims", "Anna", "Marija", "Kristaps", "Laura"};
+    private static final String[] UZVĀRDI = {"Killer", "Bērziņš", "Ozols", "Zariņš", "Lapiņš", "Sokolovs", "Kļaviņš", "Vītols"};
+    private static final String[] PICAS = {"Margarita", "Studentu", "Havajas", "Kapri", "Lauku", "Vegāniskā", "Salami", "Meksikāņu"};
+    private static final String[] UZKODAS = {"Frī kartupeļi", "Sīpolu gredzeni", "Ķiploku grauzdiņi", "Mērce", "Nekas papildus"};
 }
+    	
+    	
+    	
+    
