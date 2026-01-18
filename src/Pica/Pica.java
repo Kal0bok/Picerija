2d181b0d4674b7f307f62c5b9aec1109eec38167
Kolab0k <bogdan1ptlvt@gmail.com>
2026-01-19 01:45:46 +0300

    Izveidoju komplektaku kodu

--- src/Pica/Pica.java
+++ src/Pica/Pica.java
@@ -1,47 +1,49 @@
 package Pica;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Pica {
     private Random rand = new Random();
 
-    public static final String[] PICAS = {"Margarita", "Peperoni", "Havajas", "Studentu"};
-    public static final double[] PICAS_CENAS = {8.0, 10.0, 11.0, 7.5};
+    public static final Map<String, Double> picas = new HashMap<>();
+    public static final Map<String, Double> izmeri = new HashMap<>();
+    public static final Map<String, Double> uzkodas = new HashMap<>();
 
-    public static final String[] IZMIRI = {"25cm", "30cm", "50cm"};
-    public static final double[] IZMIRI_CENAS = {0.0, 2.0, 5.0};
+    static {
+    	picas.put("Margarita", 8.0);
+    	picas.put("Peperoni", 10.0);
+    	picas.put("Havajas", 11.0);
+    	picas.put("Studentu", 7.5);
 
-    public static final String[] MERCE = {"Kiploku", "Asa", "Kecups"};
-    public static final double MERCE_CENA = 0.50;
+    	izmeri.put("25cm", 0.0);
+    	izmeri.put("30cm", 2.0);
+    	izmeri.put("50cm", 5.0);
 
-    public static final String[] UZKODAS = {"Fri", "Sipolu gredzeni", "Nekas"};
-    public static final double[] UZKODAS_CENAS = {2.50, 3.00, 0.0};
+    	uzkodas.put("Fri", 2.50);
+    	uzkodas.put("Sipolu gredzeni", 3.00);
+    	uzkodas.put("Nekas", 0.0);
+    }
 
-    public static final String[] DZERIENI = {"Kola", "Udens", "Nekas"};
-    public static final double[] DZERIENI_CENAS = {1.50, 1.00, 0.0};
-
-    public String izveletaPica, izveletsIzmers, izveletaMerce, izveletaUzkoda, izveletsDzeriens;
+    public String izveletaPica, izveletsIzmers, izveletaUzkoda;
     public double kopejaCena = 0;
 
     public Pica() {
-        int pIdx = rand.nextInt(PICAS.length);
-        izveletaPica = PICAS[pIdx];
-        kopejaCena += PICAS_CENAS[pIdx];
+        izveletaPica = getRandomKey(picas);
+        kopejaCena += picas.get(izveletaPica);
 
-        int iIdx = rand.nextInt(IZMIRI.length);
-        izveletsIzmers = IZMIRI[iIdx];
-        kopejaCena += IZMIRI_CENAS[iIdx];
+        izveletsIzmers = getRandomKey(izmeri);
+        kopejaCena += izmeri.get(izveletsIzmers);
 
-        int mIdx = rand.nextInt(MERCE.length);
-        izveletaMerce = MERCE[mIdx];
-        kopejaCena += MERCE_CENA;
+        izveletaUzkoda = getRandomKey(uzkodas);
+        kopejaCena += uzkodas.get(izveletaUzkoda);
+    }
 
-        int uIdx = rand.nextInt(UZKODAS.length);
-        izveletaUzkoda = UZKODAS[uIdx];
-        kopejaCena += UZKODAS_CENAS[uIdx];
-
-        int dIdx = rand.nextInt(DZERIENI.length);
-        izveletsDzeriens = DZERIENI[dIdx];
-        kopejaCena += DZERIENI_CENAS[dIdx];
+    private String getRandomKey(Map<String, Double> map) {
+        List<String> keys = new ArrayList<>(map.keySet());
+        return keys.get(rand.nextInt(keys.size()));
     }
 }
